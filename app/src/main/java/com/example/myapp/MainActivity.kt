78e8eb9f7434245656f7712f5a0f3a2689f8c53c
takero8538-lgtm <takero8538@gmail.com>
2026-05-26 16:10:33 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,1 +1,27 @@
+package com.example.myapp
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var inferenceHelper: OnnxInferenceHelper
+    private lateinit var resultTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+        val inferButton: Button = findViewById(R.id.inferButton)
+
+        inferenceHelper = OnnxInferenceHelper(assets, "model1.onnx")
+
+        inferButton.setOnClickListener {
+            val result = inferenceHelper.runInference()
+            resultTextView.text = "推論結果: $result"
+        }
+    }
+}
